634643c33a63bb3787e1f8d7fcbeee311e875359
Nyx <nicola.2128@hotmail.com>
2024-11-09 13:06:02 -0500

    Initial commit

---
+++ src/main/java/niconiconyx/hungersdelight/HungersDelightClient.java
@@ -0,0 +1,11 @@
+package niconiconyx.hungersdelight;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class HungersDelightClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/niconiconyx/hungersdelight/HungersDelightDataGenerator.java
@@ -0,0 +1,11 @@
+package niconiconyx.hungersdelight;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class HungersDelightDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
